fd427ea71c78d6e5cc968ecaf5d801fa41f41b48
IndhujaPrabu <91797260+indhujaprabu@users.noreply.github.com>
2021-12-12 22:34:01 +0530

    Create Casenumtoname.sh

---
+++ Casenumtoname.sh
@@ -0,0 +1,9 @@
+read -p "enter the number from 0-5: " a
+case $a in
+        0) echo -n "zero" ;;
+        1) echo -n "One" ;;
+        2) echo -n "two" ;;
+        3) echo -n "three" ;;
+        4) echo -n "four" ;;
+        5) echo -n "five" ;;
+esac
