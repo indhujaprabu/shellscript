602cb3dd1dbc96cb96557311abdedbd2b636a9ff
IndhujaPrabu <91797260+indhujaprabu@users.noreply.github.com>
2021-12-13 19:25:12 +0530

    Create Arraysumofthree.sh

---
+++ Arraysumofthree.sh
@@ -0,0 +1,13 @@
+read -p "Enter first integer : " a
+read -p "Enter second integer : " b
+read -p "Enter third integer  : " c
+s=$(($a+$b+$c))
+echo
+
+
+if [ $s -eq 0 ]
+  then
+    echo "Sum of $a, $b, $c is Zero :" $s
+else
+   echo "Sum of $a, $b, $c is not Zero :" $s
+fi
